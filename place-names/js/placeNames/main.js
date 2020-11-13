a8235bbc2a1c1c47564edddb09f8688f0daeb78e
Ember Parr <ember.devops@gmail.com>
2020-11-13 14:55:21 -0600

    place names translated to javascript

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,13 @@
 function main() {
   // Put your code here
+  const names = ["Nashville", "Hong Kong", "The back yard", "Earth", "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+  console.log("All Place Names")
+  names.forEach(name => console.log(name))
 
+  console.log("");
+  const theNames = names.filter(n => n.startsWith("The"))
+  console.log("'The' Place Names");
+  theNames.forEach(name => console.log(name))
 }
 
 main();
