2458329a7c4f2ce6cfad4ab9ca16bfbf2bed9860
Ember Parr <ember.devops@gmail.com>
2020-11-13 15:27:57 -0600

    translated c# rolling dice to javascript

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,5 +1,23 @@
 function main() {
-  // Put your code here
+  console.log("Let's roll some dice, baby!")
+  console.log("---------------------------")
+
+  const roll = () => {
+    let dieValue = Math.floor(Math.random() * 6 + 1)
+    return dieValue
+  }
+  
+
+  for (i = 0; i < 10; i++) {
+    let die1 = roll();
+    let die2 = roll();
+    let message = `${die1} + ${die2} == ${die1 + die2}`
+    if (die1 === die2) {
+      message += "  DOUBLES!"
+    }
+
+    console.log(message)
+  }
 
 }
 
